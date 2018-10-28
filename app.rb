eebaa3df98c61c87ff7c34f7f158ae118d64c3c4
ws903 <ws903@nyu.edu>
2018-10-28 15:29:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	@name = "David"
+	@hometown = "Busan"
+	@fav_song = "Done With Love"
+
+	get('/name') {
+		"My name is #{@name}"
+	}
+
+	get('/hometown') {
+		"My hometown is #{@hometown}"
+	}
+
+	get('/favorite-song') {
+		"My favorite song is #{@fav_song}"
+	}
 end
